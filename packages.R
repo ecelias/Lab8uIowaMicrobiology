430a4309bafa41253dcb78fb174a27eee2fad4d9
Elizabeth Elias <ecelias@uiowa.edu>
2025-02-10 12:07:18 -0600

    Containerization

--- packages.R
+++ packages.R
@@ -1,1 +1,5 @@
-install.packages(c("shiny", "ggplot2", "dplyr"))
+install.packages(c("shiny", "ggplot2", "tidyverse", "BiocManager", "vegan", 
+                   "data.table", "readr", "broom", "shinycssloaders", "bslib", 
+                   "igraph", "shinyWidgets"))
+
+BiocManager::install(c("phyloseq", "phyloseqCompanion"))
